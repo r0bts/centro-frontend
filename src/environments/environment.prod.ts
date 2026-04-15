d074a132e5aa29eb9f481a24ad91d35d3fbf1be8
Roberto <roberto@centro.com>
2026-04-15 12:29:20 -0600

    chore: ignorar environment files del tracking de git

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://centro.ddev.site/api'
-  //apiUrl: 'https://back-centro-libanes.aigel.com.mx/api'
-  //apiUrl: 'https://arzsuite.centrolibanes.org.mx/api'
-
-};
